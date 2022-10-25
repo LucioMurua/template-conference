3240633543740ca417d16bbf3e2f634a54983d5c
Lucio Murua <luciomurua1992@gmail.com>
2022-10-25 11:47:01 -0300

    Form programmed 100%
    html, css and js of form has been programmed and finished.

--- js/js.js
+++ js/js.js
@@ -1,1 +1,40 @@
 var rellax = new Rellax ('.rellax');
+
+const form = document.getElementById('form');
+const nameForm = document.getElementById('name');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const message = document.getElementById('message');
+
+form.addEventListener ('submit', e=>{
+    if (nameForm.value.length < 1) {
+        nameForm.style.backgroundColor="var(--color-2)";
+        e.preventDefault();
+    }
+    else{
+        nameForm.style.backgroundColor= "var(--color-5)";
+    }
+    if (email.value.length < 1) {
+        email.style.backgroundColor="var(--color-2)";
+        e.preventDefault();
+    }
+    else{
+        email.style.backgroundColor= "var(--color-5)";
+    }
+    if (phone.value.length < 1) {
+        phone.style.backgroundColor="var(--color-2)";
+        e.preventDefault();
+    }
+    else{
+        phone.style.backgroundColor= "var(--color-5)";
+    }
+    if (message.value.length < 1) {
+        message.style.backgroundColor="var(--color-2)";
+        e.preventDefault();
+    }
+    else{
+        message.style.backgroundColor= "var(--color-5)";
+    }
+    
+    
+})
